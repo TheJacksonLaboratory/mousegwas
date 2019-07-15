ca23551798c71cf212df24922ca01e720f6add27
Asaf Peer <Asaf.Peer@jax.org>
2019-07-15 14:02:41 -0400

    a

--- exec/run_GEMMA.R
+++ exec/run_GEMMA.R
@@ -84,7 +84,7 @@ for (cn in setdiff(names(complete.geno), c("chr", "bp38", "rs", "major", "minor"
   complete.geno[..cn=='H',c(cn) := 1]
   complete.geno[..cn==major, c(cn) := 0]
   complete.geno[..cn==minor, c(cn) := 2]
-  complete.geno[,c(cn) := as.numeric(..cn)]#as.numeric(ifelse(..cn=='H', 1, ifelse(..cn==major, 0, 2)))]
+  complete.geno[,c(cn) := as.numeric(get(cn))]#as.numeric(ifelse(..cn=='H', 1, ifelse(..cn==major, 0, 2)))]
 }
 print(head(complete.geno))
 print(summary(complete.geno))
