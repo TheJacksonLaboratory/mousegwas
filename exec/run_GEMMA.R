00dc8a79c8281213a263b8128535022b2f7902e3
Asaf Peer <Asaf.Peer@jax.org>
2019-07-15 12:07:01 -0400

    a

--- exec/run_GEMMA.R
+++ exec/run_GEMMA.R
@@ -89,7 +89,7 @@ print(summary(complete.geno))
 # Compute the specific strains genomes
 strains_genomes <- srdata
 for (rnum in 1:nrow(strains)){
-  strains_genomes <- cbind(strains_genomes, strains[rnum, "input_name"]=(complete.geno[,strains[rnum, "p1"]] + complete.geno[,strains[rnum, "p2"]])/2)
+  strains_genomes[, eval(strains$input_name[rnum]):=(complete.geno[,strains$p1[rnum]] + complete.geno[,strains$p2[rnum]])/2]
 }
 fwrite(strains_genomes, "export_strains_genotypes.csv")
 # Arrange the SNPs data
