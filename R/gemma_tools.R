7a1ea7954c2433eeba0db9c022cd30baacfa79bf
Asaf Peer <Asaf.Peer@jax.org>
2019-07-24 18:12:01 -0400

    a

--- R/gemma_tools.R
+++ R/gemma_tools.R
@@ -57,7 +57,7 @@ calc_kinship <- function(genotypes, annot, exec, chrname, basedir, phenofile){
 get_residuals <- function(covars, phenotypes){
   resids <- data.table()
   for (p in names(phenotypes)){
-    lft <- lm(phenotypes[,..p] ~ covars)
+    lft <- lm(phenotypes[,get(p)] ~ covars)
     resids[,eval(p):=resid(lft)]
   }
   return(resids)
