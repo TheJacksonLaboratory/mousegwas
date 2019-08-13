8eba5245f1503be50913c93b2b6e2bb2fdaad998
Asaf Peer <Asaf.Peer@jax.org>
2019-08-13 17:46:35 -0400

    add single exec

--- R/gemma_tools.R
+++ R/gemma_tools.R
@@ -77,13 +77,14 @@ get_residuals <- function(covars, phenotypes){
 #' @param basedir dir to write files to
 #' @param eigens Number of eigenvectors to run gemma on
 #' @param loco perform LOCO (default FALSE)
+#' @param single run each phenotype separately and then run metaSOFT to combine results
 #'
 #' @return The unified output file
 #' @export
 #'
 #' @importFrom data.table merge fwrite setkey
 #' @examples
-execute_lmm <- function(genotypes, phenotypes, annot, covars, basedir, eigens, loco=TRUE){
+execute_lmm <- function(genotypes, phenotypes, annot, covars, basedir, eigens, loco=TRUE, single=TRUE){
   exec <- get_gemma(basedir)
   # Set keys and merge the genotypes and annotations
   #setkey(genotypes, rs, physical = FALSE)
@@ -104,21 +105,36 @@ execute_lmm <- function(genotypes, phenotypes, annot, covars, basedir, eigens, l
   genofile <- paste0(basedir, "/all_genotypes.csv")
   fwrite(genotypes, genofile, col.names = FALSE, na = "NA")
 
-  # Write the phenotype files
-#  for (n in range(dim(phenotypes)[2])){
-#    fwrite(phenotypes[,n,with=FALSE], paste0(basedir,"/phenotype_",n,".csv"), col.names=FALSE, sep=",")
-#  }
+  if (single){
+    # Write the phenotype files
+    for (n in range(dim(phenotypes)[2])){
+      fwrite(phenotypes[,n,with=FALSE], paste0(basedir,"/phenotype_",n,".csv"), col.names=FALSE, sep=",")
+    }
+  }
+
   # Compute lmm without LOCO
   if (!loco){
     system(paste0("cd ", basedir, " && ", exec, " -g ", genofile,
                   " -p ", phenofile, " -gk 1 -o kinship_all"))
     ksfile <- paste0(basedir, "/output/kinship_all.cXX.txt")
-    system(paste0("cd ", basedir, " && ", exec, " -lmm 2 -g ", genofile,
-                  " -p ", phenofile, " -a ", anotfile,
-                  " -c ", covarfile,
-                  " -k ", ksfile, " -o lmm_all",
-                  " -n ", do.call(paste, c(as.list(1:dim(phenotypes)[2], sep=" ")))))
-    return(paste0(basedir,"/output/lmm_all.assoc.txt"))
+    if (single){
+      for (n in 1:dim(phenotypes)[2]){
+        pfile <- paste0(basedir,"/phenotype_",n,".csv")
+        system(paste0("cd ", basedir, " && ", exec, " -lmm 2 -g ", genofile,
+                      " -p ", pfile, " -a ", anotfile,
+                      " -c ", covarfile,
+                      " -k ", ksfile, " -o lmm_all_phenotype_", n,
+                      " -n 1"))
+      }
+
+    }else{
+      system(paste0("cd ", basedir, " && ", exec, " -lmm 2 -g ", genofile,
+                    " -p ", phenofile, " -a ", anotfile,
+                    " -c ", covarfile,
+                    " -k ", ksfile, " -o lmm_all",
+                    " -n ", do.call(paste, c(as.list(1:dim(phenotypes)[2], sep=" ")))))
+      return(paste0(basedir,"/output/lmm_all.assoc.txt"))
+    }
   }else{
     # Compute kinship to each chromosome and run gemma with loco
     for (chrname in unique(annot$chr)){
@@ -126,23 +142,44 @@ execute_lmm <- function(genotypes, phenotypes, annot, covars, basedir, eigens, l
       geno_sfile <- paste0(basedir, "/genotypes_only_chr_", chrname, ".csv")
       fwrite(genotypes[genotypes$rs %in% annot[annot$chr==chrname,"rs"],], geno_sfile, col.names=FALSE, na="NA")
      # for (n in range(dim(phenotypes)[2])){
+      if (single){
+        pfiles <- c(phenofile)
+        outfiles <- c(paste0("lmm_", chrname, "_allpheno"))
+        nns <- do.call(paste, c(as.list(1:eigens, sep=" ")))
+      }else{
+        pfiles <- sapply(1:dim(phenotypes)[2], function(n) paste0(basedir,"/phenotype_",n,".csv"))
+        outfiles <- sapply(1:dim(phenotypes)[2], function(n) paste0(
+          "lmm_", chrname, "_pheno_", n))
+        nns <- "1"
+      }
 
-      print(paste0("Executing: cd ", basedir, " && ", exec, " -lmin 0.01 -lmax 100 -lmm 2 -g ", geno_sfile,
-                   " -p ", phenofile, " -a ", anotfile,
-                   " -k ", ksfile, " -o lmm_", chrname,
-                   " -n ", do.call(paste, c(as.list(1:eigens, sep=" ")))))
-      system(paste0("cd ", basedir, " && ", exec, " -lmin 0.01 -lmax 100 -lmm 2 -g ", geno_sfile,
-                    " -p ", phenofile, " -a ", anotfile,
-                    #" -c ", covarfile,
-                    " -k ", ksfile, " -o lmm_", chrname,
-                    " -n ", do.call(paste, c(as.list(1:eigens, sep=" ")))))
-                    #" -n 1"))# do.call(paste, c(as.list(1:dim(phenotypes)[2], sep=" ")))))
-      #}
+      for (n in 1:length(pfiles)){
+        print(paste0("Executing: cd ", basedir, " && ", exec, " -lmin 0.01 -lmax 100 -lmm 2 -g ", geno_sfile,
+                     " -p ", pfiles[n], " -a ", anotfile,
+                     " -k ", ksfile, " -o ", outfiles[n],
+                     " -n ", nns))
+        system(paste0("cd ", basedir, " && ", exec, " -lmin 0.01 -lmax 100 -lmm 2 -g ", geno_sfile,
+                      " -p ", pfiles[n], " -a ", anotfile,
+                      #" -c ", covarfile,
+                      " -k ", ksfile, " -o ", outfiles[n],
+                      " -n ", nns))
+      }
+      # If singles combine the results to one file
+      if (length(pfiles)>1){
+        cmass <- fread(paste0(outfiles[1], ".assoc.txt"))
+        for (n in 2:length(outfiles)){
+          ctmp <- fread(paste0(outfiles[n], ".assoc.txt"))[,.(rs, p_lrt)]
+
+          cmass <- merge(cmass, ctmp, by="rs", all=T, suffixes("", paste0(".", outfiles[n])))
+        }
+        cmass[paste0("p_lrt.", outfiles[1]) := p_lrt]
+        fwrite(cmass, file=paste0("lmm_", chrname, "_allpheno.assoc.txt"))
+      }
     }
     # Concatenate all loco files into a single output file
     system(paste0("cd ", basedir,
-           " && cat output/lmm*.assoc.txt |head -1 > output/all_lmm_associations.assoc.txt",
-           " && cat output/lmm*.assoc.txt |grep -v allele >> output/all_lmm_associations.assoc.txt"))
+           " && cat output/lmm*allpheno.assoc.txt |head -1 > output/all_lmm_associations.assoc.txt",
+           " && cat output/lmm*allpheno.assoc.txt |grep -v allele >> output/all_lmm_associations.assoc.txt"))
     return(paste0(basedir, "/output/all_lmm_associations.assoc.txt"))
   }
 }
