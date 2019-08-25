c9226a0f311f79ba55d640f3aac1cb5e4b16216d
Asaf Peer <Asaf.Peer@jax.org>
2019-08-25 13:02:34 -0400

    a

--- R/gemma_tools.R
+++ R/gemma_tools.R
@@ -139,7 +139,7 @@ execute_lmm <- function(genotypes, phenotypes, annot, covars, basedir, eigens, l
     for (n in 1:dim(phenotypes)[2]){
       print(n)
       print(head(phenotypes))
-      print(head(as.dats.table(phenotypes[,n,with=FALSE])))
+      print(head(as.data.table(phenotypes[,n,with=FALSE])))
       fwrite(as.data.table(phenotypes[,n,with=FALSE]), paste0(basedir,"/phenotype_",n,".csv"), col.names=FALSE, sep=",")
     }
   }else{
