26c876aec7949b6b9368a33350496b5ab0cb5d59
Asaf Peer <Asaf.Peer@jax.org>
2019-11-18 14:07:06 -0500

    a

--- exec/run_GEMMA.R
+++ exec/run_GEMMA.R
@@ -310,7 +310,8 @@ pval_thr <- args$snpthr
 
 # Manhattan plot
 p <- plot_gemma_lmm(results_file, genes=genes, name=args$header, metasoft=is.metasoft, pyLMM=args$method=="pyLMM" && ncol(b$phenotypes)==1,
-                    annotations=paste0(args$basedir, "/annotations.csv"), namethr=args$namethr, redthr=pval_thr)
+                    annotations=paste0(args$basedir, "/annotations.csv"), namethr=args$namethr, redthr=pval_thr,
+                    genotypes = paste0(args$basedir, "/all_genotypes.csv"))
 ggsave(paste0(args$basedir, "/manhattan_plot_p_lrt.pdf"), plot=p$plot, device="pdf", width=16, height=8, units="in")
 
 # Read the significant SNPs and grab their related genes
