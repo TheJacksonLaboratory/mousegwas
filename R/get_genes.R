85c2f244083e418e5a99d9288c44cf414955dabe
Asaf Peer <Asaf.Peer@jax.org>
2019-11-28 18:13:26 -0500

    a

--- R/get_genes.R
+++ R/get_genes.R
@@ -16,14 +16,16 @@ get_genes <- function(snps, dist=1000000){
   # Get the genes from biomaRt
   library(biomaRt)
   library(fuzzyjoin)
-  library(tibble)
+  library(tidyverse)
   ensembl <- useMart("ensembl", dataset="mmusculus_gene_ensembl")
   annot <- getBM(c("ensembl_gene_id", "mgi_symbol", "chromosome_name", "strand", "start_position", "end_position",
                  "gene_biotype"), mart=ensembl)
   annot$ext_start <- annot$start_position-dist
   annot$ext_end <- annot$end_position+dist
   annot <- as_tibble(annot) %>% filter(!is.na(chromosome_name), !is.na(ext_start), !is.na(ext_end))
-  affgene <- fuzzy_left_join(as_tibble(snps), annot, by=c("chr"="chromosome_name", "ps"="ext_start", "ps"="ext_end"), match_fun=c("==", ">=", "<="))
+  affgene <- left_join(snps, annot, by = c("chr" = "chromosome_name")) %>%
+    filter(ps >= ext_start, ps <= ext_end)
+  #affgene <- fuzzy_left_join(as_tibble(snps), annot, by=c("chr"="chromosome_name", "ps"="ext_start", "ps"="ext_end"), match_fun=c("==", ">=", "<="))
   # There should probably be a better solution but let's do it with a for loop
   #affgene <- data.frame(rs=character(), gene=character())
   #for (s in 1:nrow(snps)){
