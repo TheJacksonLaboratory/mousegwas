9f048b9bd730cb7879e6d92f7cda13f8e9bf40bc
Asaf Peer <Asaf.Peer@jax.org>
2020-01-20 21:07:21 -0500

    a

--- R/get_genes.R
+++ R/get_genes.R
@@ -4,6 +4,7 @@
 #'
 #' @param snps A list of SNPs with chr, ps columns
 #' @param dist Distance of genes from SNPs
+#' @param attempts Maximal number of attempts to try biomaRt
 #'
 #' @return
 #' @export
@@ -11,12 +12,20 @@
 #' @examples
 #' @import biomaRt
 #' @import tibble
-get_genes <- function(snps, dist=1000000){
+get_genes <- function(snps, dist=1000000, attempts=5){
   # Get the genes from biomaRt
   #library(biomaRt)
-  ensembl <- biomaRt::useMart("ensembl", dataset="mmusculus_gene_ensembl")
-  annot <- biomaRt::getBM(c("ensembl_gene_id", "mgi_symbol", "chromosome_name", "strand", "start_position", "end_position",
-                 "gene_biotype"), mart=ensembl)
+  annot <- NULL
+  attn <- 1
+  while (attn <= attempts && is.null(annot)){
+    attn <- attn + 1
+    try(
+        ensembl <- biomaRt::useMart("ensembl", dataset="mmusculus_gene_ensembl"))
+    try(
+        annot <- biomaRt::getBM(c("ensembl_gene_id", "mgi_symbol", "chromosome_name", "strand", "start_position", "end_position",
+                       "gene_biotype"), mart=ensembl)
+    )
+  }
   annot$ext_start <- annot$start_position-dist
   annot$ext_end <- annot$end_position+dist
   # Input might have minps/maxps or just ps
