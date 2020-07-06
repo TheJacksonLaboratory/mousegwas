891aeaebf6ade7fd4a00b33f76ff79b891acfcfe
Asaf Peer <Asaf.Peer@jax.org>
2020-07-06 15:46:20 -0400

    a

--- exec/postprocess_mv.R
+++ exec/postprocess_mv.R
@@ -197,7 +197,9 @@ if (args$meanvariance) {
       pveplot$var_plot + scale_fill_manual(values = grpcol) + theme_bw()  +
         theme(
           text = element_text(size = 10, family = ffam),
-          legend.position = "none"
+          legend.position = "none",
+          axis.text.x = element_blank(),
+          axis.ticks.x = element_blank()
         ),
       nrow = 1,
       ncol = 2
