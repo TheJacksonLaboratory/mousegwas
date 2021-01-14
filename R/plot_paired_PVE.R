18a8c20751dd2533d2641b5058a19e58cc25630d
Asaf Peer <Asaf.Peer@jax.org>
2021-01-14 06:49:17 -0500

    a

--- R/plot_paired_PVE.R
+++ R/plot_paired_PVE.R
@@ -24,7 +24,7 @@ paired_PVE_plot <- function(PVE, var_name = "Variance", minherit=0){
     ggplot(varPVE, aes(reorder(basename, -meanPVE$PVE), PVE, fill = Group, alpha = PVE>minherit)) + geom_bar(color =
                                                                                       "black", stat = "identity") +
     geom_errorbar(aes(ymin = PVE - PVESE, ymax = PVE + PVESE), width = .2) + ylim(0,1) +
-    scale_alpha_manual(values=c(0.6, 1))
+    scale_alpha_manual(values=c(0.6, 1)) +
     xlab("") + coord_flip() + ggtitle("Variance")
 
   return(list(mean_plot = mean_pvep, var_plot = var_pvep))
